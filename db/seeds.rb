e0995fe9e3e148a6dfcf3f878b406440fe515bb9
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-24 16:48:43 -0700

    updated seeds, config file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
 
 require 'faker'
+20.times do
+  User.create!(
+  email: Faker::Internet.unique.email,
+  password: "password",
+  password_confirmation: "password",
+  role: 'standard',
+  stripe_customer_id: 'stripe_customer_id'
+  )
+end
+users = User.all
+
+seed_user = User.create(
+  email: "admin@admin.com",
+  password: "password",
+  password_confirmation: "password",
+  role: "standard",
+  stripe_customer_id: "stripe_customer_id"
+)
 
 10.times do
   Wiki.create!(
@@ -12,4 +30,5 @@ end
 wikis = Wiki.all
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Wiki.count} wikis created"
