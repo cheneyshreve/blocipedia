d25311f46ca0b9a2809410b2c09933b10c32ca57
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-03-24 17:16:45 -0700

    added SendGrid for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,20 +21,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_options = { from: "shedevsnow@gmail.com" }
   config.action_mailer.default_url_options = { :host => 'https://immense-ocean-13499.herokuapp.com'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-  }
+ActionMailer::Base.smtp_settings = {
+  :address          => "smtp.sendgrid.net",
+  :port             => "25",
+  :authentication   => :plain,
+  :user_name        => ENV['SENDGRID_USERNAME'],
+  :password         => ENV['SENDGRID_PASSWORD']
+}
+
 
   config.active_record.dump_schema_after_migration = false
 
